6faaa894d96adfebe176bf3899d640906f767e4b
Mauro Donnantuoni Moratto <87683709+OldOwl83@users.noreply.github.com>
2021-12-28 15:42:18 -0300

    Corrección validación string y encapsulamiento
    Se introdujo una corrección importante en la validación del string (fórmulas de la forma "!(p) & (q)" eran invalidadas).
    Se procedió a encapsular métodos y propiedades, con el objetivo de introducir futuros setters y getters.

--- logprop.py
+++ logprop.py
@@ -40,7 +40,7 @@ from pandas import options
 #Configuración para la salida de las tablas de verdad (librería pandas)
 options.display.max_rows = 128
 options.display.max_columns = 40
-options.display.width = 100
+options.display.width = 120
 
 '''
 Clase general: aunque técnicamente no es la clase base de los objetos que
@@ -57,7 +57,7 @@ particular (tanto para la fórmula principal como para sus subfórmulas)
 class FBF:
     def __new__(cls, form_str):  # La instanciación del objeto devuelve un objeto de tipo diferente ('Formula')
         # Descompone el string en sus elementos componentes (conectiva principal y fórmulas miembro)
-        elems_form = cls.Descomponer_string(form_str)
+        elems_form = cls._Descomponer_string(form_str)
 
         if(elems_form != -1):  # Valida si la fórmula es correcta y determina su tipo, pasando sus elementos componentes como argumentos al constructor correspondiente
             match elems_form[1]:
@@ -70,14 +70,14 @@ class FBF:
         else: raise Exception("Fórmula inválida") # Si la fórmula no es validada, levanta una excepción
 
 # Método de descomposición del string en que se expresa la fórmula
-    def Descomponer_string(form_str):
+    def _Descomponer_string(form_str):
 
         subforms = ["", "", ""] # Almacena los elementos componentes de la fórmula expresada en el string (subfórmula izq, conectiva/tipo, [subfórmula der])
 
         form_str = form_str.replace(' ', '') # Limpia los espacios en blanco
 
 # Función para validar los elementos definidos por paréntesis exteriores. Esta función complementa el análisis hecho con las expresiones regulares, ya que las mismas no pueden controlar la paridad entre paréntesis izquierdos y derechos dentro de un elemento interno de la fórmula. Devuelve el elemento sin los paréntesis externos, o -1 si no es validado
-        def Tomar_parentesis(string):
+        def _Tomar_parentesis(string):
             p_izq = 0  # Contadores de paréntesis izquierdos y derechos
             p_der = 0
             pos = 0 # Contador de ciclos
@@ -89,12 +89,14 @@ class FBF:
                 if char == '(': p_izq += 1
                 elif char == ')':
                     p_der += 1
-                # Cuando los paréntesis derechos son tantos como los izquierdos (pero más que cero), y coinciden con los extremos del elementos, se completó la validación
-                    if p_der == p_izq and p_der > 0 and pos == len(string): return string[1:-1]  # Devuelve el elemento sin paréntesis exteriores
+                # Cuando los paréntesis derechos son tantos como los izquierdos (pero más que cero), y coinciden con los extremos del elemento, se completó la validación
+                    if p_der == p_izq and p_der > 0:
+                        if pos == len(string): return string[1:-1]  # Devuelve el elemento sin paréntesis exteriores
+                        else: return -1 #Si el elemento es más largo, no está bien formado.
 
             return -1
 
-#(Continúa "Descomponer_string()")
+#(Continúa "_Descomponer_string()")
         #Busca determinar la estructura general de la fórmula, con expresiones regulares
 
         #Pregunta si la fórmula es una variable proposicional aislada
@@ -103,7 +105,7 @@ class FBF:
             subforms[1] = 'at'
 
         #Pregunta si la fórmula es una negación
-        elif fullmatch("![A-Za-z]+", form_str) or fullmatch("!\([A-Za-z()&|\->!]+\)", form_str):
+        elif fullmatch("![A-Za-z]+", form_str) or (fullmatch("!\([A-Za-z()&|\->!]+\)", form_str) and _Tomar_parentesis(form_str[1:]) != -1 and len(_Tomar_parentesis(form_str[1:])) + 3 == len(form_str)):
             if form_str[1] == '(': subforms[0] = form_str[2:-1]
             else: subforms[0] = form_str[1:]
             subforms[1] = '!'
@@ -111,17 +113,16 @@ class FBF:
         #Pregunta si la fórmula está compuesta por dos subfórmulas coordinadas por una conectiva central
         else:
             fr_patt = fullmatch("^(!{0,1}\([A-Za-z()&|\->!]+\))(&|->|\|)(!{0,1}\([A-Za-z()&|\->!]+\))$", form_str) or fullmatch("^(!{0,1}\([A-Za-z()&|\->!]+\))(&|->|\|)(!{0,1}[A-Za-z]+)$", form_str) or fullmatch("^(!{0,1}[A-Za-z]+)(&|->|\|)(!{0,1}\([A-Za-z()&|\->!]+\))$", form_str) or fullmatch("^(!{0,1}[A-Za-z]+)(&|->|\|)(!{0,1}[A-Za-z]+)$", form_str)
-
             #Registra los tres elementos en la lista 'subforms', validando los elementos que están encerrados entre paréntesis (los demás ya son controlados por las expresiones regulares)
             if fr_patt:
                 if fr_patt.group(1)[0] == '(':
-                    subforms[0] = Tomar_parentesis(fr_patt.group(1))
+                    subforms[0] = _Tomar_parentesis(fr_patt.group(1))
                     if subforms[0] == -1: return -1
                 else:
                     subforms[0] = fr_patt.group(1)
 
                 if fr_patt.group(3)[0] == '(':
-                    subforms[2] = Tomar_parentesis(fr_patt.group(3))
+                    subforms[2] = _Tomar_parentesis(fr_patt.group(3))
                     if subforms[2] == -1: return -1
                 else:
                     subforms[2] = fr_patt.group(3)
@@ -141,75 +142,78 @@ La superclase que engloba a la fórmula principal y sus subfórmulas. Todas ella
 '''
 class Formula:
     def __init__(self, subf1, subf2=None):
-        self.subform1 = FBF(subf1) #Todas las subfórmulas están compuestas por al menos otra subfórmula, salvo las atómicas (F_ATOM), que tienen un constructor especial
+        self._subform1 = FBF(subf1) #Todas las subfórmulas están compuestas por al menos otra subfórmula, salvo las atómicas (F_ATOM), que tienen un constructor especial
         if subf2:
-            self.subform2 = FBF(subf2)
+            self._subform2 = FBF(subf2)
         else:
-            self.subform2 = None
+            self._subform2 = None
 
     def __str__(self): pass #El casting a string se define de manera particular en las subclases
 
+    def __repr__(self):
+        return "FBF('{}')".format(str(self))
+
 #Método que devuelve para cada subfórmula su nivel de profundidad en relación a la fórmula que lo llamó
-    def Devolver_profundidad(self):
-        if self.subform2: #La profundidad de toda subfórmula puede calcularse como la mayor profundidad alcanzada por sus subfórmulas, más uno (teniendo en cuenta que siempre las subfórmulas atómicas están en el primer nivel)
-            prof_acum = max(self.subform1.Devolver_profundidad(),
-                            self.subform2.Devolver_profundidad())
+    def _Devolver_profundidad(self):
+        if self._subform2: #La profundidad de toda subfórmula puede calcularse como la mayor profundidad alcanzada por sus subfórmulas, más uno (teniendo en cuenta que siempre las subfórmulas atómicas están en el primer nivel)
+            prof_acum = max(self._subform1._Devolver_profundidad(),
+                            self._subform2._Devolver_profundidad())
         else:
-            prof_acum = self.subform1.Devolver_profundidad()
+            prof_acum = self._subform1._Devolver_profundidad()
         return prof_acum + 1
 
-#Método que devuelve la sucesión de todas las subfórmulas que componen la fórmula que lo llamó. Aun está sin uso... para futuras aplicaciones.
-    def Imprimir_arbol(self):
-        self.subform1.Imprimir_arbol()
-        if self.subform2:
-            self.subform2.Imprimir_arbol()
-        print(self, end=" | ")
-
 #Método recursivo que devuelve el valor de verdad de cada subfórmula, dependiendo del valor asignado a las variables proposicionales. Tiene una definición especial en cada subclase.
-    def Tomar_valor(self): pass
+    def _Tomar_valor(self): pass
+
+#Función recursiva que recorre el árbol de subfórmulas hasta alcanzar las variables proposicionales que coinciden con el string "var", para poder asignarles el mismo valor de verdad "val".
+    def _Asignar_valores(self, var, val):
+        if type(self) == F_ATOM:
+            if self._subform1 == var:
+                self._valor = val
+        else:
+            self._subform1._Asignar_valores(var, val)
+            if self._subform2:
+                self._subform2._Asignar_valores(var, val)
+
+#Método que devuelve la sucesión de todas las subfórmulas que componen la fórmula que lo llamó. Aun está sin uso... para futuras aplicaciones.
+    # def Imprimir_arbol(self):
+    #     self._subform1.Imprimir_arbol()
+    #     if self._subform2:
+    #         self._subform2.Imprimir_arbol()
+    #     print(self, end=" | ")
 
 #Método que construye la tabla de verdad completa de la fórmula que lo llamó (incluyendo los valores de sus subfórmulas), y la imprime por pantalla.
     def Generar_tabla(self):
         val_forms = dict() #Almacena para el string que representa cada subfórmula (keys) su nivel de profundidad y los valores de verdad que le corresponden en cada fila de la tabla.
 
-    #Función que registra el nombre (string) de todas las subfórmulas como "keys" del diccionario 'val_forms', y le asocia su respectivo nivel de profundidad, y una lista vacía que se irá completando con los valores de verdad que le corresponden en cada fila de la tabla.
-        def Registro_formulas(self):
+    #Función interna que registra el nombre (string) de todas las subfórmulas como "keys" del diccionario 'val_forms', y le asocia su respectivo nivel de profundidad, y una lista vacía que se irá completando con los valores de verdad que le corresponden en cada fila de la tabla.
+        def __Registro_formulas(self):
             nonlocal val_forms
 
             val_forms.update(
-                {str(self): [self.Devolver_profundidad(), list()]})
+                {str(self): [self._Devolver_profundidad(), list()]})
 
             if type(self) != F_ATOM:
-                Registro_formulas(self.subform1)
-                if self.subform2:
-                    Registro_formulas(self.subform2)
+                __Registro_formulas(self._subform1)
+                if self._subform2:
+                    __Registro_formulas(self._subform2)
 
-    #Función recursiva que recorre el árbol de subfórmulas hasta alcanzar las variables proposicionales que coinciden con el string "var", para poder asignarles el mismo valor de verdad "val".
-        def Asignar_valores(self, var, val):
-            if type(self) == F_ATOM:
-                if self.subform1 == var:
-                    self.valor = val
-            else:
-                Asignar_valores(self.subform1, var, val)
-                if self.subform2:
-                    Asignar_valores(self.subform2, var, val)
-
-    #Función recursiva que recorre el árbol de subfórmulas para tomar su valor de acuerdo al valor establecido en las variables proposicionales, y lo almacena en las listas de valores correspondiente a cada una de ellas en el diccionario "val_forms".
-        def Registrar_valores_fila(self, list_rep):
+    #Función recursiva interna que recorre el árbol de subfórmulas para tomar su valor de acuerdo al valor establecido en las variables proposicionales, y lo almacena en las listas de valores correspondiente a cada una de ellas en el diccionario "val_forms".
+        def __Registrar_valores_fila(self, list_rep):
             for forms in val_forms.keys():
                 if str(self) == forms and not forms in list_rep:
-                    val_forms[forms][1].append(self.Tomar_valor()) #Almacena el valor correspondiente a la subfórmula particular.
+                    val_forms[forms][1].append(self._Tomar_valor()) #Almacena el valor correspondiente a la subfórmula particular.
                     list_rep.add(forms) # Set para controlar que los valores de verdad no se dupliquen cuando hay dos subfórmulas iguales.
 
                     break
             # Y llama al resto del árbol
             if type(self) != F_ATOM:
-                Registrar_valores_fila(self.subform1, list_rep)
-                if self.subform2:
-                    Registrar_valores_fila(self.subform2, list_rep)
+                __Registrar_valores_fila(self._subform1, list_rep)
+                if self._subform2:
+                    __Registrar_valores_fila(self._subform2, list_rep)
 
 #(continúa '.Generar_tabla()')
-        Registro_formulas(self) # Registro del árbol de subfórmulas en el diccionario "val_forms".
+        __Registro_formulas(self) # Registro del árbol de subfórmulas en el diccionario "val_forms".
 
         var_logs = [val for val in val_forms.keys() if val_forms[val][0] == 1] # Almacena las variables proposicionales, que reconoce por su nivel de profundidad.
 
@@ -219,16 +223,16 @@ class Formula:
                 periodo = (2 ** len(var_logs)) / divisor #Calcula el período de variación
 
                 if (fila // periodo) % 2 == 0: # Asigna verdadero o falso
-                    Asignar_valores(self, var, True)
+                    self._Asignar_valores(var, True)
                 else:
-                    Asignar_valores(self, var, False)
+                    self._Asignar_valores(var, False)
                 divisor *= 2
 
-            Registrar_valores_fila(self, set()) # Asigna los valores del resto de las subfórmulas.
+            __Registrar_valores_fila(self, set()) # Asigna los valores del resto de las subfórmulas.
 
         tabla = dict() #Diccionario donde se ordenan las subfórmulas por nivel de profundidad y se elimina este dato; de modo que quede apto para construir un "DataFrame" de "pandas".
 
-        for prof in range(1, self.Devolver_profundidad() + 1):
+        for prof in range(1, self._Devolver_profundidad() + 1):
             for form, val in val_forms.items():
                 if val[0] == prof:
                     tabla.update({form: val[1]})
@@ -243,10 +247,10 @@ class Formula:
 class F_CONJ(Formula):
 
     def __str__(self):
-        return "(" + str(self.subform1) + " & " + str(self.subform2) + ")"
+        return "(" + str(self._subform1) + " & " + str(self._subform2) + ")"
 
-    def Tomar_valor(self):
-        if self.subform1.Tomar_valor() and self.subform2.Tomar_valor():
+    def _Tomar_valor(self):
+        if self._subform1._Tomar_valor() and self._subform2._Tomar_valor():
             return True
         else:
             return False
@@ -255,10 +259,10 @@ class F_CONJ(Formula):
 class F_DISY(Formula):
 
     def __str__(self):
-        return "(" + str(self.subform1) + " | " + str(self.subform2) + ")"
+        return "(" + str(self._subform1) + " | " + str(self._subform2) + ")"
 
-    def Tomar_valor(self):
-        if self.subform1.Tomar_valor() or self.subform2.Tomar_valor():
+    def _Tomar_valor(self):
+        if self._subform1._Tomar_valor() or self._subform2._Tomar_valor():
             return True
         else:
             return False
@@ -267,10 +271,10 @@ class F_DISY(Formula):
 class F_COND(Formula):
 
     def __str__(self):
-        return "(" + str(self.subform1) + " -> " + str(self.subform2) + ")"
+        return "(" + str(self._subform1) + " -> " + str(self._subform2) + ")"
 
-    def Tomar_valor(self):
-        if not self.subform1.Tomar_valor() or self.subform2.Tomar_valor():
+    def _Tomar_valor(self):
+        if not self._subform1._Tomar_valor() or self._subform2._Tomar_valor():
             return True
         else:
             return False
@@ -279,10 +283,10 @@ class F_COND(Formula):
 class F_NEG(Formula):
 
     def __str__(self):
-        return '!' + str(self.subform1)
+        return '!' + str(self._subform1)
 
-    def Tomar_valor(self):
-        if not self.subform1.Tomar_valor():
+    def _Tomar_valor(self):
+        if not self._subform1._Tomar_valor():
             return True
         else:
             return False
@@ -291,10 +295,10 @@ class F_NEG(Formula):
 class F_DUP(Formula):
 
     def __str__(self):
-        return "(" + str(self.subform1) + ")"
+        return "(" + str(self._subform1) + ")"
 
-    def Tomar_valor(self):
-        if self.subform1.Tomar_valor():
+    def _Tomar_valor(self):
+        if self._subform1._Tomar_valor():
             return True
         else:
             return False
@@ -303,20 +307,20 @@ class F_DUP(Formula):
 class F_ATOM(Formula):
 
     def __init__(self, subf1):
-        self.subform1 = subf1 # A diferencia del resto de las subclases de "Formula", la subfórmula que compone a F_ATOM no es ella misma un objeto "Formula", sino un string; de modo de evitar una recursión infinita.
+        self._subform1 = subf1 # A diferencia del resto de las subclases de "Formula", la subfórmula que compone a F_ATOM no es ella misma un objeto "Formula", sino un string; de modo de evitar una recursión infinita.
 
-        self.valor = None # Otra diferencia de F_ATOM es que su valor de verdad se define de manera absoluta, mientras que el del resto se define por relación a éste.
+        self._valor = None # Otra diferencia de F_ATOM es que su valor de verdad se define de manera absoluta, mientras que el del resto se define por relación a éste.
 
     def __str__(self):
-        return self.subform1
+        return self._subform1
 
-    def Imprimir_arbol(self):
-        print(self, end=" | ")
+    #def Imprimir_arbol(self):
+    #    print(self, end=" | ")
 
-    def Devolver_profundidad(self): #También la profundidad de F_ATOM se puede decir que es absoluta.
+    def _Devolver_profundidad(self): #También la profundidad de F_ATOM se puede decir que es absoluta.
         return 1
 
-    def Tomar_valor(self):
-        return self.valor
+    def _Tomar_valor(self):
+        return self._valor
 
 #======================= FIN del MÓDULO 'logprop' =========================
